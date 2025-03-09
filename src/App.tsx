dc95c7d0c66333bd32cc25158720ab25f999d0e7
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-03-09 18:52:05 -0300

    Adicionando variáveis css e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
-       
+       <h1>
+        Hello World!
+       </h1>
+
+       <button>
+        Click here!
+       </button>
+
+       <button disabled>
+        Don't click here! 
+       </button>
     </>
   )
 }
